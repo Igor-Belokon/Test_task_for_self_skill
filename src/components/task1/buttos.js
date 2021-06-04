46ee3b42c247a8d1887902a0ffb6670c271e121b
Igor-Belokon <igorbelokon8@gmail.com>
2021-06-04 11:29:14 +0300

    add animeited border

--- src/components/task1/buttos.js
+++ src/components/task1/buttos.js
@@ -4,12 +4,17 @@ export default ({ onClick, units = [] }) => {
   return (
     <div className="buttons-conteiner">
       {units.map((unit) => (
-        <button
-          className="button-comp"
+        <div
+          class="svg-wrapper"
           onClick={() => onClick(unit.unit_name, unit.koef_to_m)}
         >
-          {unit.unit_name}
-        </button>
+          <svg height="60" width="100" xmlns="http://www.w3.org/2000/svg">
+            <rect class="shape1" height="60" width="100" />
+            <rect class="shape2" height="60" width="100" />
+
+            <div class="text"> {unit.unit_name} </div>
+          </svg>
+        </div>
       ))}
     </div>
   );
